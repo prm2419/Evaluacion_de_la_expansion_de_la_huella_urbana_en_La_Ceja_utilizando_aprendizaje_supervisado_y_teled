ece676690061d33b85e8cf9e3e663cc1047c5306
AndyM220 <161912497+AndyM220@users.noreply.github.com>
2025-07-07 12:32:28 -0500

    Stacks generation for each date

--- Obtener_satcks.py
+++ Obtener_satcks.py
@@ -44,5 +44,5 @@ if __name__ == "__main__":
     base_dir = "./carpeta_combinado"   # Carpeta que contiene subcarpetas YYYY-MM-DD
     out_dir  = "./carpeta_stacks"      # Carpeta donde se guardarán los stacks
 
-    date = "2019-01-04" # Se corre el código a una sola fecha
+    date = "2023-08-26" # Se corre el código a una sola fecha
     make_index_stack(date, base_dir, out_dir)
